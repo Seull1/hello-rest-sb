02adde285139664fdb583520efaad0a388de4884
seulle <seull@LAPTOP-L9AOK14Q.>
2024-02-27 16:07:04 +0900

    test

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nohup java -jar ~/app/sb-rest/rest-service-0.1.0-SNAPSHOT.jar >> ~/app/sb-rest/log.log &
+nohup java -jar ~/app/sb-rest/rest-service-0.0.1-SNAPSHOT.jar >> ~/app/sb-rest/log.log &
 echo $! > ~/app/sb-rest/pid.file
